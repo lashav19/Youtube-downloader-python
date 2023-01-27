f64e903d466e98a968e68565e58364cae4d652c2
B4Z1C <37025351+ArtigKarPlayZ@users.noreply.github.com>
2023-01-27 14:29:47 +0100

    Update Youtube downloader modern.py

--- Youtube downloader modern.py
+++ Youtube downloader modern.py
@@ -1,17 +1,37 @@
 import customtkinter
+from pytube import YouTube
 from PIL import Image
 import tkinter
 from tkinter import messagebox
 import os
-#from pytube import YouTube
+
 
 #shortcuts for widgets ex: Button(master=app, text="Press me!")
 Label = customtkinter.CTkLabel
 Button = customtkinter.CTkButton
 Entry = customtkinter.CTkEntry
 
-def downloadVideo():
-    pass
+def downloadVideo(video_link: str, path: str, extension=str):
+
+    if video_link == "":
+        errorWindow(442, "Please input video link")
+        return
+    video = YouTube(video_link)
+    video_download = None
+    if extension == "mp4":
+        print("downloading mp4")
+        video_download = video.streams.get_highest_resolution()
+        video_download.download(str(path))
+
+    if extension == "mp3":
+        print("Downloading mp3")
+        video_download = video.streams.filter(only_audio=True).first().on_progress()
+        out_file = video_download.download(str(path), )
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+
+
 
 def fileBrowse():
     browse = customtkinter.filedialog.askdirectory(initialdir=fr"C:/Users/{os.getlogin()}/Downloads")
@@ -50,7 +70,7 @@ if __name__ == "__main__":
     dirButton = Button(app, text="", image=customtkinter.CTkImage(folder), fg_color="transparent", width=25, command=fileBrowse)
     dirButton.place(relx=0.67, rely=0.35, anchor=tkinter.CENTER)
 
-    start = Button(app, text="Download video", width=250, command=downloadVideo)
+    start = Button(app, text="Download video", width=250, command=downloadVideo())
     start.place(relx=0.5, rely=0.45, anchor=tkinter.CENTER)
 
 
