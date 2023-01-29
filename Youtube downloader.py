499b4561692eb3368f5b6d176dddb48b45b4ee13
B4Z1C <artigpokemon@gmail.com>
2023-01-29 15:21:23 +0100

    Update Youtube downloader
    Made a separate function for downloading the youtube video, might add it to the getImageURL file later so it won't take up space in the main file

--- Youtube downloader.py
+++ Youtube downloader.py
@@ -23,23 +23,33 @@ def downloader():  # Downloads the youtube video
         return
     messagebox.showinfo(
         "Heads up", "The program will freeze sometimes, just give it some time")
+
     video = YouTube(link.get())
-    url = video.thumbnail_url
-    thumbnail = getImage(url)
-    thumb = Picture(thumbnail, size=(250, 200))
-    tn = Label(app, text="", image=thumb, height=40, width=150)
+
+    urlImage = getImage(video.thumbnail_url)
+    showThumbnail = Picture(urlImage, size=(250, 200))
+
+    tn = Label(app, text="", image=showThumbnail, height=40, width=150)
     tn.place(relx=0.2, rely=0.4, anchor=tkinter.CENTER)
+
     app.update_idletasks()
 
-# Checks which filetyope is used
-    video_download = None
-    if filetype.get() == "mp4":
-        video_download = video.streams.get_highest_resolution()
-        video_download.download(str(directory.get()))
 
-    if filetype.get() == "mp3":
-        video_download = video.streams.filter(only_audio=True).first()
-        out_file = video_download.download(str(directory.get()))
+    download_video(video, filetype.get(), directory.get())  # calls function to download the video
+
+    sleep(1)
+    messagebox.showinfo("Download complete", "Sucessfully downloaded video")
+
+
+def download_video(video: YouTube, file_type: str, download_dir: str):
+    # Checks which filetype is used
+    video_download = None
+    if file_type == "mp4":  # Downloads highest mp4 resolution
+        video.streams.get_highest_resolution().download(download_dir)
+
+    if file_type == "mp3":  #
+        audio_stream = video.streams.filter(only_audio=True).first()
+        out_file = audio_stream.download(download_dir)
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
         # Renaming the file to mp3 because it downloads as a "mp4" file with no video
@@ -59,21 +69,17 @@ def downloader():  # Downloads the youtube video
                     print(f"Retry -> Name = {name}")
                     name += 1
 
-        sleep(1)
-    messagebox.showinfo("Download complete", "Sucessfully downloaded video")
-
 
 # Opens a file browser where you select your downloads directory, defaults to the standard windows directory
 def fileBrowse():
     browse = customtkinter.filedialog.askdirectory(initialdir=fr"C:/Users/{os.getlogin()}/Downloads")
+
     # updates the text in the directory to include the string text of which directory you chose
     browse = customtkinter.StringVar(app, browse)
     directory.configure(textvariable=browse)
 
-# Shows error window, takes error code (title of window) and message  to display
 
-
-def errorWindow(errorCode, message):
+def errorWindow(errorCode, message):  # Shows error window, takes error code (title of window) and message  to display
     messagebox.showerror(errorCode, message)
 
 
@@ -82,26 +88,34 @@ if __name__ == "__main__":
     # sets the theme of the app
     customtkinter.set_appearance_mode("dark")
 
+
     # define app and values
     app = customtkinter.CTk()
     app.title("Youtube Downloader")
     app.geometry("1000x700")
 
+
     # widgets
     l1 = Label(app, text="Youtube downloader", font=("Helvetica", 30))
     l1.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
+
 
     link = customtkinter.CTkEntry(
         app, placeholder_text="Youtube link", width=300)
     link.place(relx=0.5, rely=0.35, anchor=tkinter.CENTER)
 
-    # folder png
-    scriptDir = os.path.dirname(os.path.abspath(__file__))
-    imgpath = os.path.join(scriptDir, "Exe", 'folder.png')
-    folder = Image.open(imgpath)
+
+    # Failsafe for both testing and production code so no mixups happen
+    try:
+        scriptDir = os.path.dirname(os.path.abspath(__file__))
+        imgpath = os.path.join(scriptDir, "Exe", 'folder.png')
+        folder = Image.open(imgpath)
+    except FileNotFoundError:
+        folder = Image.open("folder.png")
 
     directory = Entry(app, placeholder_text="Download directory", width=300)
     directory.place(relx=0.5, rely=0.45, anchor=tkinter.CENTER)
+
 
     # button that calls the fileBrowse function
     dirButton = Button(app, text="", image=customtkinter.CTkImage(
